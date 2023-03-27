0ab81e253d4a72b0dbf8806054100445bb03d5ac
fadeonsp <edfadeon@gmail.com>
2023-03-27 15:13:45 -0300

    enviando para github

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,15 @@
+export const dataFake = [
+
+  {
+    "id": '1',
+    "title": 'Angular Libs',
+    "description": 'A estrutura do Angular é formada por um pequeno núcleo,que contém as funções principais do framework, e umabiblioteca de componentes que, ao serem agregados à aplicação,permitem a execução de inúmeros recursos adicionais',
+    "photoCover": 'https://vidafullstack.com.br/wp-content/uploads/2021/07/versao-12-angular.png'
+  },
+  {
+    "id": '2',
+    "title": 'Angular HTTP',
+    "description": 'Fazedo resquições http',
+    "photoCover": 'https://amoelcodigo.com/wp-content/uploads/2020/08/Angular-1024x576.png'
+  }
+]
